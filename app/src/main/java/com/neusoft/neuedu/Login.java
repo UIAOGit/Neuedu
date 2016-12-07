28a6e467e88537c5fe65d431c9c5ae59123312d7
UIAOGit <yusitaomail@163.com>
2016-12-07 11:01:43 +0800

    loginView

--- app/src/main/java/com/neusoft/neuedu/Login.java
+++ app/src/main/java/com/neusoft/neuedu/Login.java
@@ -2,12 +2,34 @@ package com.neusoft.neuedu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+    private EditText username,password,captcha;
+    private TextView refresh;
+    private ImageView captchaImage;
+    private CheckBox checkBox;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        findById();
+        
+    }
+    public void findById()
+    {
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        captcha = (EditText) findViewById(R.id.captcha);
+        refresh = (TextView) findViewById(R.id.refresh);
+        captchaImage = (ImageView) findViewById(R.id.captcha_image);
+        checkBox = (CheckBox) findViewById(R.id.checkBox);
+        login = (Button) findViewById(R.id.login);
     }
 }
