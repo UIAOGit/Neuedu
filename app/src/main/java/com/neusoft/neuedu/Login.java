b73757e6512d812047e6135673415aa043f7c7fd
UIAOGit <yusitaomail@163.com>
2016-12-09 09:27:37 +0800

    captcha

--- app/src/main/java/com/neusoft/neuedu/Login.java
+++ app/src/main/java/com/neusoft/neuedu/Login.java
@@ -1,20 +1,40 @@
 package com.neusoft.neuedu;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+import com.neusoft.neuedu.data.GetData;
 
-public class Login extends AppCompatActivity {
+
+public class Login extends AppCompatActivity implements View.OnClickListener{
     private EditText username,password,captcha;
+    private Bitmap bitmap;
     private TextView refresh;
     private ImageView captchaImage;
     private CheckBox checkBox;
     private Button login;
-
+    private String userName,passWord,CAPTCHA;
+    private Handler handler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+            switch (msg.what) {
+                case 0x001:
+                    captchaImage.setImageBitmap(bitmap);
+                    Toast.makeText(Login.this, "验证码已加载", Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,5 +51,34 @@ public class Login extends AppCompatActivity {
         captchaImage = (ImageView) findViewById(R.id.captcha_image);
         checkBox = (CheckBox) findViewById(R.id.checkBox);
         login = (Button) findViewById(R.id.login);
+        refresh.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.login:
+                userName = username.getText().toString();
+                passWord = password.getText().toString();
+                CAPTCHA = captcha.getText().toString();
+                break;
+            case R.id.refresh:
+                final String path = "http://www.neuedu.cn/imgcode";
+                new Thread() {
+                    public void run() {
+                        try {
+                            byte[] data = GetData.getImage(path);
+                            bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                        handler.sendEmptyMessage(0x001);
+                    }
+                }.start();
+                break;
+
+        }
     }
 }
